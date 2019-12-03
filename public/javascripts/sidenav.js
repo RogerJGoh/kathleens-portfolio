a56530e313eeb104ca8081df1554565852c80853
Roger <rogerjgoh@gmail.com>
2019-12-03 22:41:50 +0800

    concept art images

--- public/javascripts/sidenav.js
+++ public/javascripts/sidenav.js
@@ -1,6 +1,6 @@
 //sidenav
 $("#sidenav_trigger").on("mouseenter",function(e){
-    document.getElementById("sidenav").style.width = "80px";
+    document.getElementById("sidenav").style.width = "100px";
 });
 
 $("#sidenav_trigger").on("mouseleave",function(e){
