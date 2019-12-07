4819f58a92593fd8218ac9cc66efcc1fa7ae52d7
Roger <rogerjgoh@gmail.com>
2019-12-07 14:32:36 +0800

    index page format done

--- public/javascripts/sidenav.js
+++ public/javascripts/sidenav.js
@@ -1,8 +1,8 @@
 //sidenav
-$("#sidenav_trigger").on("mouseenter",function(e){
+$("#sidenav_trigger").on("click",function(e){
     document.getElementById("sidenav").style.width = "100px";
 });
 
-$("#sidenav_trigger").on("mouseleave",function(e){
-    document.getElementById("sidenav").style.width = "0px";
-});
+// $("#sidenav_trigger").on("mouseleave",function(e){
+//     document.getElementById("sidenav").style.width = "0px";
+// });
