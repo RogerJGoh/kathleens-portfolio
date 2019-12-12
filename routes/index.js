85a3d45d5bdfa6acd049228221f5e72010d41e53
Roger <rogerjgoh@gmail.com>
2019-12-12 14:23:00 +0800

    fairy tale format fixes

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Kat's Portfolio" });
+  res.render('index', { title: "Kathleen Nita" });
 });
 
 router.get('/about_me', function(req, res, next) {
-  res.render('about me', { title: "Kat's Portfolio" });
+  res.render('about me', { title: "Kathleen Nita" });
 });
 
 router.get('/commission_work', function(req, res, next) {
-  res.render('commission work', { title: "Kat's Portfolio" });
+  res.render('commission work', { title: "Kathleen Nita" });
 });
 
 router.get('/not_a_fairy_tale', function(req, res, next) {
-  res.render('not a fairy tale', { title: "Kat's Portfolio" });
+  res.render('not a fairy tale', { title: "Kathleen Nita" });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: "Kat's Portfolio" });
+  res.render('contact', { title: "Kathleen Nita" });
 });
 
 module.exports = router;
